a671103ce7e836f41fe256a634f04626573efcc5
shineyr <fly_yr2012@163.com>
2015-09-25 10:45:54 +0800

    变量的定义和使用

---
+++ 03_var.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#testing variables
+days=10
+guest="Katie"
+echo "$guest checked in $days days ago"
+days=5;
+guest="Jessica"
+echo "$guest checked in $days days ago"
+
+var1=10
+var2=$var1
+echo The resulting value is $var2
