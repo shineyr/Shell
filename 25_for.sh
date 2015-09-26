4de5a09d319a035bd89d6fadba81adf4aae7addf
shineyr <fly_yr2012@163.com>
2015-09-26 09:23:31 +0800

    结构化命令for

---
+++ 25_for.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+# basic for command
+
+#基本格式如下：
+# for var in list
+# do
+# 	commands
+# done
+
+for test in Alabama Alaska Arizona Arkansas California Colorado
+do
+	echo The next state is $test
+done
+
+
