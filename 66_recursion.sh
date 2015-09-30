b3d41fb38c6d0a98bbd9259c8f3ca25021bcdb0f
shineyr <fly_yr2012@163.com>
2015-09-30 10:12:45 +0800

    递归实现求阶乘

---
+++ 66_recursion.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# using recursion 使用递归实现求阶乘
+
+function factorial {
+	if [ $1 -eq 1 ]
+	then
+		echo 1
+	else
+		local temp=$[ $1 - 1 ]
+		local result=`factorial $temp`
+		echo $[ $result * $1 ]
+	fi
+}
+
+read -p "Enter value:" value
+result=`factorial $value`
+echo "The factorial of $value is: " $result
+
+
+
